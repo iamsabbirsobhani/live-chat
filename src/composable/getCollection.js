07cca917ff96eee6fd45bdc8947f112795320dfa
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-02-18 20:02:03 +0600

    Updated: Unsubscribing From Real-Time Listeners

--- src/composable/getCollection.js
+++ src/composable/getCollection.js
@@ -31,7 +31,7 @@ const getCollection = (collection) => {
     })
     watchEffect((onInvalidate) => {
         onInvalidate(() => {
-            unsub
+            unsub()
         })
     })
     return {

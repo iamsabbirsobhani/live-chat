4c5ef3082d78b60f10864c0a1e68af1527a451a2
Sabbir Sobhani <sabbirsobhani@hotmail.com>
2021-05-02 10:51:49 +0600

    bug fixed

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <el-page-header style="margin: 10px" @back="goBack" content="Profile">
   </el-page-header>
-  <h1 style="text-align: center; font-size: 25px;">Home</h1>
+  <h1 style="text-align: center; font-size: 25px">Home</h1>
   <div v-for="doc in formattedDocuments" :key="doc.userUid" class="postcard">
     <el-card v-if="doc.post" shadow="always">
       <router-link
@@ -20,20 +20,19 @@
       </router-link>
       <p class="post">{{ doc.post }}</p>
       <div class="feeling">
-      <Button
-        @click.stop="like(doc.id, user.uid)"
-        icon="pi pi-thumbs-up"
-        class="p-button-rounded p-button-text"
-      />
-      <p>{{ doc.like }}</p>
-      <Button
-        @click.stop="dislike(doc.id, user.uid)"
-        style="color: red"
-        icon="pi pi-thumbs-down"
-
-        class="p-button-rounded p-button-danger p-button-text"
-      />
-      <p style="display: inline;">{{ doc.dislike }}</p>
+        <Button
+          @click.stop="like(doc.id, user.uid)"
+          icon="pi pi-thumbs-up"
+          class="p-button-rounded p-button-text"
+        />
+        <p>{{ doc.like }}</p>
+        <Button
+          @click.stop="dislike(doc.id, user.uid)"
+          style="color: red"
+          icon="pi pi-thumbs-down"
+          class="p-button-rounded p-button-danger p-button-text"
+        />
+        <p style="display: inline">{{ doc.dislike }}</p>
       </div>
     </el-card>
   </div>
@@ -45,22 +44,21 @@ import getPosts from "@/composable/getPosts.js";
 import getUsers from "@/composable/getUsers.js";
 import { useRouter } from "vue-router";
 import Button from "primevue/button";
-import { computed, watch } from "vue";
+import { computed } from "vue";
 import { format } from "date-fns";
-import likeSystem from '@/composable/likeSystem.js'
-import dislikeSystem from '@/composable/dislikeSystem.js'
+import likeSystem from "@/composable/likeSystem.js";
+import dislikeSystem from "@/composable/dislikeSystem.js";
 
 export default {
   components: { Button },
   setup() {
     const { user } = getUser();
     const { error, documents } = getUsers();
-    // const { status } = getPostForHome('posts')
     const { statusHome } = getPosts("posts");
     const router = useRouter();
 
-    const { likePost } = likeSystem()
-    const { dislikePost } = dislikeSystem()
+    const { likePost } = likeSystem();
+    const { dislikePost } = dislikeSystem();
 
     const formattedDocuments = computed(() => {
       if (statusHome.value) {
@@ -76,17 +74,12 @@ export default {
     };
 
     const like = (postId, reacter) => {
-      likePost(postId, reacter)
-      return false
-    }
+      likePost(postId, reacter);
+    };
 
     const dislike = (postIdt, reactert) => {
-      // console.log(postId)
-      // console.log(reacter)
-      dislikePost(postIdt, reactert)
-      return false
-
-    }
+      dislikePost(postIdt, reactert);
+    };
 
     return { goBack, user, formattedDocuments, like, dislike };
   },
@@ -95,24 +88,20 @@ export default {
 
 <style lang="scss" scoped>
 @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400&family=Roboto:wght@100&display=swap");
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400&display=swap");
 .postcard {
   max-width: 500px;
   margin: 10px auto;
 }
 .post {
-    font-size: 16px;
-
-  // font-family: "Montserrat", sans-serif;
-  font-family: 'Roboto', sans-serif;
-
+  font-size: 16px;
+  font-family: "Roboto", sans-serif;
 }
 .name {
   display: flex;
   align-items: center;
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
   font-size: 14px;
-
 }
 .name h3 {
   margin-left: 10px;
@@ -133,10 +122,10 @@ export default {
   align-items: center;
 }
 
-.feeling p{
+.feeling p {
   margin: 10px;
   color: gray;
-  // font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
 }
 
 @media (max-width: 425px) {

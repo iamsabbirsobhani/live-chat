e453142ff3cd1de0e96044d9000251e3486c0bb4
iamsabbirsobhani <sabbirsobhani@hotmail.com>
2021-11-26 17:46:57 +0600

    Notification styled

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,15 +1,29 @@
-importScripts('https://www.gstatic.com/firebasejs/8.2.7/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/8.2.7/firebase-messaging.js')
+importScripts("https://www.gstatic.com/firebasejs/8.2.7/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.7/firebase-messaging.js");
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDohSvFlyf-DxbRKNo_sofoqGNKcUbD-Sg",
-    authDomain: "live-chat-backup-server-28985.firebaseapp.com",
-    databaseURL:
-      "https://live-chat-backup-server-28985-default-rtdb.firebaseio.com",
-    projectId: "live-chat-backup-server-28985",
-    storageBucket: "live-chat-backup-server-28985.appspot.com",
-    messagingSenderId: "204927114230",
-    appId: "1:204927114230:web:0b8fefb98eb9b2d1b245d7",
-  };
+  apiKey: "AIzaSyDohSvFlyf-DxbRKNo_sofoqGNKcUbD-Sg",
+  authDomain: "live-chat-backup-server-28985.firebaseapp.com",
+  databaseURL:
+    "https://live-chat-backup-server-28985-default-rtdb.firebaseio.com",
+  projectId: "live-chat-backup-server-28985",
+  storageBucket: "live-chat-backup-server-28985.appspot.com",
+  messagingSenderId: "204927114230",
+  appId: "1:204927114230:web:0b8fefb98eb9b2d1b245d7",
+};
 
-const app = firebase.initializeApp(firebaseConfig)
+const app = firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging(); // for receiving data from server & notification style as well
+
+//   messaging.onMessageReceived(function(payload) {
+//     console.log('Received background message ', payload);
+
+//     const notificationTitle = payload;
+//     const notificationOptions = {
+//       body: payload,
+//     };
+
+//     self.registration.showNotification(notificationTitle,
+//       notificationOptions);
+//   });
